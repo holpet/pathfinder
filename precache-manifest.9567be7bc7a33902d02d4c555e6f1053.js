5bbb532edbd2b9b1a5e0ab2c20e01bc7a1f7e298
holpet <45787742+holpet@users.noreply.github.com>
2020-10-01 22:50:23 +0200

    Updates

---
+++ precache-manifest.9567be7bc7a33902d02d4c555e6f1053.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd283ee460b1e5e16d61b67fea900ef",
+    "url": "/pathfinder/index.html"
+  },
+  {
+    "revision": "6958b76eec6a7a66824f",
+    "url": "/pathfinder/static/css/main.3df9da20.chunk.css"
+  },
+  {
+    "revision": "154ede6e25be761faff8",
+    "url": "/pathfinder/static/js/2.b354800c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/pathfinder/static/js/2.b354800c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6958b76eec6a7a66824f",
+    "url": "/pathfinder/static/js/main.0cfc5688.chunk.js"
+  },
+  {
+    "revision": "0479df8af0bab6308eed",
+    "url": "/pathfinder/static/js/runtime-main.17d2ccb5.js"
+  },
+  {
+    "revision": "f7c48a77750062fdb0fbe9eecd266864",
+    "url": "/pathfinder/static/media/dot_full.f7c48a77.svg"
+  },
+  {
+    "revision": "7228ac6aca10b7e28ef1f1d538985d41",
+    "url": "/pathfinder/static/media/flag.7228ac6a.svg"
+  },
+  {
+    "revision": "608d80cd9ff10a24f7f52a0faf13917d",
+    "url": "/pathfinder/static/media/hold.608d80cd.svg"
+  },
+  {
+    "revision": "d9d208a8fdf6b1d9faf33baae414944f",
+    "url": "/pathfinder/static/media/start.d9d208a8.svg"
+  }
+]);
